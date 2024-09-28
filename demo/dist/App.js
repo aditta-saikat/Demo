40b6505d3e3bbca9cdabdc00e7ee9dec9146e70d
aditta-saikat <u1904121@student.cuet.ac.bd>
2024-09-28 14:40:21 +0600

    folder renamed

--- demo/dist/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './i18n';
-import Home from "./Component/Home/Home";
-const App = () => {
-    return (_jsx(Router, { children: _jsx(Routes, { children: _jsx(Route, { path: "/", element: _jsx(Home, {}) }) }) }));
-};
-export default App;
-//# sourceMappingURL=App.js.map
